6210a6a4184edc250af044a49ff6e3357a8b0371
Praise Freedom Dike <dikepraise119@gmail.com>
2025-02-13 20:16:41 +0100

    refactor: remove console log from Supabase client initialization

--- src/lib/API/Services/init/supabase.ts
+++ src/lib/API/Services/init/supabase.ts
@@ -4,7 +4,5 @@ export const SupabaseServerClient = () => {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
   const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
-  console.log('supabaseUrl', supabaseUrl);
-
   return createClient(supabaseUrl, supabaseAnonKey);
 };
