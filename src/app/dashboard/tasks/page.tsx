f4169ee96802e0da46fb69dbb85a8b7acf3a642f
Praise Freedom Dike <dikepraise119@gmail.com>
2025-03-06 09:38:35 +0100

    feat: update header title in TasksPage to reflect daily tasks

--- src/app/dashboard/tasks/page.tsx
+++ src/app/dashboard/tasks/page.tsx
@@ -40,7 +40,7 @@ export default function TasksPage() {
 
   return (
     <section className="py-4 lg:px-16 bg-card text-card-foreground">
-      <h2 className="text-2xl font-bold mb-4">My Goals & Tasks</h2>
+      <h2 className="text-2xl font-serif font-bold mb-4">Daily Tasks </h2>
 
       <GoalSettingModal isOpen={showGoalModal} onClose={handleGoalSetComplete} />
 
